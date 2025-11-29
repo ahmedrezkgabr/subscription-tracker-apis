4e35836ea33d3a820140c385fdfd7b1106b1adcc
rezk <ahmedrezkofficial@gmail.com>
2025-11-29 21:28:40 +0200

    redefine routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,8 +4,8 @@ import { signIn, signUp, singOut } from '../controllers/auth.controller.js';
 const authRouter = Router();
 
 // path: /api/v1/auth
-authRouter.post('/sign-up', signUp);
-authRouter.post('/sign-in', signIn);
-authRouter.post('/sign-out', singOut);
+authRouter.route('/sign-up').post(signUp);
+authRouter.route('/sign-in').post(signIn);
+authRouter.route('/sign-out').post(singOut);
 
 export default authRouter;
