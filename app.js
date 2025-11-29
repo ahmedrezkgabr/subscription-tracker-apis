0b3f1c40013bcde059ca79f31051772f5b110cf3
rezk <ahmedrezkofficial@gmail.com>
2025-11-29 17:59:58 +0200

    implement error handling middleware

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 
 import { PORT } from './config/env.js';
 
@@ -6,18 +7,29 @@ import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.routes.js';
 import subscriptionRouter from './routes/subscription.router.js';
 import connectDB from './database/mongodb.js';
+import errorMiddleware from './middlewares/error.middleware.js';
 
+// Initialize Express app
 const app = express();
-app.use(express.json());
 
+// Middleware setup
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(cookieParser());
+
+// Route setup
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
+
+// Error handling middleware
+app.use(errorMiddleware);
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
+// Start the server
 app.listen(PORT, async () => {
   console.log(`Subdub API is running on http://localhost:${PORT}`);
   await connectDB();
--- config/env.js
+++ config/env.js
@@ -1,3 +1,12 @@
 import { config } from 'dotenv';
 config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` }); // to handle multiple env files
-export const { PORT, NODE_ENV, DB_HOST, DB_USER, DB_PASS, DB_URI } = process.env;
+export const {
+  PORT,
+  NODE_ENV,
+  DB_HOST,
+  DB_USER,
+  DB_PASS,
+  DB_URI,
+  JWT_SECRET,
+  JWT_EXPIRES_IN,
+} = process.env;
