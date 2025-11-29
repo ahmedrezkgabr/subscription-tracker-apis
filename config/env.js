89f062b1b29e5589f5025a243e405f8124819e63
rezk <ahmedrezkofficial@gmail.com>
2025-11-29 14:35:31 +0200

    set-up config

---
+++ config/env.js
@@ -0,0 +1,3 @@
+import { config } from 'dotenv';
+config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` }); // to handle multiple env files
+export const { PORT, NODE_ENV, DB_HOST, DB_USER, DB_PASS } = process.env;
